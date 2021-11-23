afbd3651c48a64b113b261db322747f39d7d6d4a
Kevin <84983282+Cernical@users.noreply.github.com>
2021-11-23 18:45:29 +0100

    Update main-0.1.0.py

--- main-0.1.0.py
+++ main-0.1.0.py
@@ -6,6 +6,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 import time
 import subprocess #Necesario para usar el sistema y sus funciones
@@ -21,17 +22,18 @@ class MathGame(App):
         #Widgets de cabecera añadidos en el plano horizontal
         cabecera = BoxLayout(orientation ='horizontal')
 
-        consola = Label(text = "¡Bienvenido a MathGame! v0.12.3")
+        consola = Label(text = "¡Bienvenido a MathGame Android Edition! v0.1.0")
 
         cabecera.add_widget(consola)
 
         #Widgets de pie de página añadidos en el plano vertical
         pie = BoxLayout(orientation ='vertical')
 
-        bienvenida = Label(text = "Seleccione dificultad")
+        bienvenida = Label(text = "Seleccione dificultad:")
 
         def callback(instance):
             dificultadSeleccionada = instance.text #contiene el string del boton
+            print(instance.text)
 
         dificultadFacil = Button(text = "Fácil")
         dificultadFacil.bind(on_press=callback)
@@ -56,6 +58,5 @@ class MathGame(App):
 
 #Creacion de la raiz del programa
 root = MathGame()
-
 #Llamada al constructor
 root.run()
